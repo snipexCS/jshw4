fdff9508133919a291465ecb608ea30af6207493
snipexCS <123857302+snipexCS@users.noreply.github.com>
2023-03-08 11:32:17 +0500

    Add files via upload

---
+++ mait.js
@@ -0,0 +1,74 @@
+let time=+prompt("время")
+switch(time){
+case 1 :
+alert(time+" часа ночи ");
+break 
+case 2 :
+alert(time+" часа ночи ");
+break
+case 3 :
+alert(time+" часа ночи ");
+break
+case 4 :
+alert(time+" часа ночи ");
+break
+case 5 :
+alert(time+" часа ночи ");
+break
+case 6 :
+alert(time+" часа ночи ");
+break
+case 7:
+alert(time+" часов утра");
+break
+case 8:
+alert(time+" часов утра");
+break
+
+case 9:
+alert(time+" часов утра");
+break
+
+case 10:
+alert(time+"  утра");
+break
+case 11:
+alert(time+" часа дня");
+break
+case 12:
+alert(time+" часа дня");
+break
+case 13:
+alert(time+" часа дня");
+break
+case 14:
+alert(time+" часа дня");
+break
+case 15:
+alert(time+" часа дня");
+break
+case 16:
+alert(time+" часа дня");
+break
+case 17:
+alert(time+" часа дня");
+break
+case 18:
+alert(time+" часов вечера");
+break
+case 19:
+alert(time+" часов вечера");
+break
+case 20:
+alert(time+" часов вечера");
+break
+case 21:
+alert(time+" часов вечера");
+break
+case 22:
+alert(time+" часов вечера");
+break
+case 23:
+alert(time+" часов вечера");
+break
+}
